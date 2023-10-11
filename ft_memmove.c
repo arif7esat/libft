5136e830c207cffa534f9f128f9c9c002f717d5d
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-11 10:24:17 +0300

    Libft Kütüphanesi Güncellemesi:
    Bu güncelleme, Libft kütüphanesine yeni bir işlev ekler: ft_memmove. Bu işlev, bellek alanları arasında güvenli bir şekilde veri kopyalamak için kullanılır.
    
    ft_memmove:
    ft_memmove, bellek kopyalama işlemi sırasında çakışmaları önler. Bellek alanları birbirine üst üste bindiğinde veya çakıştığında dahi güvenli bir şekilde veri kopyalamak için kullanılır.
    Bu yeni işlev, özellikle bellek yönetimi gerektiren uygulamalarda kullanılmak üzere eklenmiştir. Libft kütüphanesi, C programlarının daha güvenli ve güçlü olmasına katkıda bulunur.

---
+++ ft_memmove.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 23:42:11 by arif              #+#    #+#             */
+/*   Updated: 2023/10/11 07:21:39 by agungor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	if (!dst && !src)
+		return (NULL);
+	if (dst < src)
+		ft_memcpy(dst, src, n);
+	else if (dst > src)
+		while (n--)
+			*((unsigned char *)(dst + n)) = *((unsigned char *)(src + n));
+	return (dst);
+}
