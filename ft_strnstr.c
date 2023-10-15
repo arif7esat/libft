6204f8a7f360c10289e6f8bdea3850e9c3ae8f9e
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-16 01:22:26 +0300

    var olan iki foksiyon arasındaki bağlantının oluşması için ufak bir trik olduğunu anladım ve gerekli düzenlemeler yapıldı...

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 11:33:13 by agungor           #+#    #+#             */
-/*   Updated: 2023/10/14 11:34:01 by agungor          ###   ########.fr       */
+/*   Updated: 2023/10/16 01:18:33 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	len_needle;
+	size_t	needle_len;
 
-	len_needle = ft_strlen(needle);
-	if (!len_needle)
+	needle_len = ft_strlen(needle);
+	if (!needle_len)
 		return ((char *)haystack);
-	while (len >= len_needle)
+	while (*haystack && len >= needle_len)
 	{
-		len--;
-		if (!(ft_memcmp(haystack, needle, len_needle)))
+		if (!ft_strncmp(haystack, needle, needle_len))
 			return ((char *)haystack);
+		len--;
 		haystack++;
 	}
 	return (NULL);
