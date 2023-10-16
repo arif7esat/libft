2ff5653339c28860bddf0c35e6b4c277c1414a89
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-17 01:03:32 +0300

    yazılan ilk fonksiyondaki ilk if ifadesinin şartı ve return ettiği durum düzeltildi

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,10 @@
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 08:28:38 by agungor           #+#    #+#             */
-/*   Updated: 2023/10/17 00:56:36 by agungor          ###   ########.fr       */
+/*   Updated: 2023/10/17 01:02:07 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -18,17 +17,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*my_substr;
 	size_t	max_len;
 
-	if (!s && len >= ft_strlen(s) && len == 0)
-		return (NULL);
+	if (!s || start >= ft_strlen(s) || !len)
+		return (ft_strdup(""));
 	max_len = ft_strlen(s + start);
 	if (len > max_len)
 		len = max_len;
 	my_substr = malloc(sizeof(char) * (len + 1));
-	if (!(my_substr))
+	if (!my_substr)
 		return (NULL);
 	return (ft_strlcpy(my_substr, s + start, len + 1), my_substr);
 }
-*/
 
 /*
 #include "libft.h"
@@ -98,6 +96,7 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 }
 */
 
+/*
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -115,8 +114,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		result = (char *)malloc(len + 1);
 		if (!result)
 			return (NULL);
-		ft_strlcpy(result, &s[start], len + 1);
-		return (result);
+		return (ft_strlcpy(result, &s[start], len + 1), result);
 	}
 	return (NULL);
 }
+*/
