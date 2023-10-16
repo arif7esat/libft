f3ab8f785eb3b7583db3704a82cb8aa359cfcad7
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-17 02:35:06 +0300

    elimizdeki tüm kömürleri nasıl elmasa dönüştürdüm

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 00:03:12 by agungor           #+#    #+#             */
-/*   Updated: 2023/10/12 00:49:46 by agungor          ###   ########.fr       */
+/*   Updated: 2023/10/16 20:12:19 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t		i;
-	const char	*str;
+	size_t	i;
+	char	*str;
 
 	i = -1;
 	str = (char *)s;
 	while (++i < n)
-		if (str[i] == c)
+		if (str[i] == (char)c)
 			return ((void *)&str[i]);
 	return (NULL);
 }
