382cf1c0888b4e2f6b273dffd157a7e22fdf7b96
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-18 00:43:29 +0300

    sadece son 2 return için farklı 2 gösterim olduğunu göstermek istedim.

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 09:57:09 by agungor           #+#    #+#             */
-/*   Updated: 2023/10/16 01:51:55 by agungor          ###   ########.fr       */
+/*   Updated: 2023/10/18 00:42:34 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,21 @@ char	*ft_strrchr(const char *s, int c)
 	len = ft_strlen(s) + 1;
 	while (--len >= 0)
 		if (s[len] == (char)c)
+			return ((char *)s + len);
+	return (NULL);
+}
+
+/*
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	len;
+
+	len = ft_strlen(s) + 1;
+	while (--len >= 0)
+		if (s[len] == (char)c)
 			return ((char *)&s[len]);
 	return (0);
 }
+*/
