53a74823d271a9f24bf1c2224c938eb9345a7de8
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-18 03:03:02 +0300

    buradaki linked listler

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 23:50:17 by agungor           #+#    #+#             */
+/*   Updated: 2023/10/18 01:25:50 by agungor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*result;
+
+	result = (t_list *)malloc(sizeof(t_list));
+	if (!result)
+		return (NULL);
+	result->content = content;
+	result->next = NULL;
+	return (result);
+}
